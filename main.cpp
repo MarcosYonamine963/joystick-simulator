87290b6c459d8786c89d746ec70f5f121450e87f
MarcosYonamine963 <marcos.m.yonamine@gmail.com>
2023-11-17 16:52:09 -0300

    new files

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+/*
+    Joystick simulator to control the selection of Icons inside a menu.
+
+    Created by: Marcos Yonamine
+    Nov. 2023
+    Rev: 0.0.0
+
+    Obs: To compile run this command on linux terminal:
+
+        g++ -o main main.cpp joystick.cpp icons.cpp
+
+    than, to execute the program:
+
+        ./main
+
+*/
+
 #include <iostream>
 using std::cin;
 using std::cout;
@@ -5,5 +22,12 @@ using std::endl;
 
 #include "joystick.hpp"
 #include "icons.hpp"
+
+
+int main()
+{
+    cout << "Running the application" << "\n";
+    return 0;
+}
 
 
---
+++ screen.hpp
@@ -0,0 +1,11 @@
+#ifndef SCREEN_HPP
+#define SCREEN_HPP
+
+/* Class to contain joystick and icons */
+
+/* Maybe must reorganize other classes as subclasses of this */
+
+/* Or this as subclass of other */
+
+
+#endif
